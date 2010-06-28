15265c5f328dbe53c7fb0d09375b9990235c6da7
shaunsephton <shaunsephton@gmail.com>
2010-06-28 09:10:36 +0200

    added twitter tags

--- social/templatetags/social_inclusion_tags.py
+++ social/templatetags/social_inclusion_tags.py
@@ -11,3 +11,24 @@ def facebook_login_button(context):
         'form_id': "facebook_login_form_%s" % random.randint(1,1000)
     })
     return context
+
+@register.inclusion_tag('social/inclusion_tags/facebook_register_button.html', takes_context=True)
+def facebook_register_button(context):
+    context.update({
+        'form_id': "facebook_register_form_%s" % random.randint(1,1000),
+    })
+    return context
+
+@register.inclusion_tag('social/inclusion_tags/twitter_login_button.html', takes_context=True)
+def twitter_login_button(context):
+    context.update({
+        'form_id': "twitter_login_form_%s" % random.randint(1,1000),
+    })
+    return context
+
+@register.inclusion_tag('social/inclusion_tags/twitter_register_button.html', takes_context=True)
+def twitter_register_button(context):
+    context.update({
+        'form_id': "twitter_register_form_%s" % random.randint(1,1000),
+    })
+    return context

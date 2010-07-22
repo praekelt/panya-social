a71b59ab1f9fdfa3f695b130e5b117be63734931
shaunsephton <shaunsephton@gmail.com>
2010-07-22 17:02:04 +0200

    added remove and done

--- social/views.py
+++ social/views.py
@@ -2,8 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import Http404
 from django.shortcuts import render_to_response
+from django.template import RequestContext
     
-from friends.models import FriendshipInvitation
+from friends.models import Friendship, FriendshipInvitation
 
 
 @login_required
@@ -21,3 +22,25 @@ def add_friend(request, user_id):
     FriendshipInvitation.objects.create_friendship_request(from_user=requesting_user, to_user=user)
    
     return render_to_response('social/inclusion_tags/friendship_awaiting_confirmation_button.html', {'object': user})
+
+@login_required
+def remove_friend(request, user_id):
+    """
+    Ajax view used in conjunction with the friendship_setup_button tag to remove an existing friendship and return 
+    the same tag with updated friend relation.
+    """
+    if not request.is_ajax():
+        raise Http404
+    
+    user = User.objects.get(id=user_id)
+    requesting_user = request.user
+
+    Friendship.objects.remove(user1=requesting_user, user2=user)
+   
+    return render_to_response('social/inclusion_tags/friendship_add_button.html', {'object': user})
+    
+    
+@login_required
+def invitation_confirm_done(request):
+    context = RequestContext(request)
+    return render_to_response('social/invitation_confirm_done.html', context)

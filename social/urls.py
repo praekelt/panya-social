87b1b058c27bdcf9ccd620f02e55544f60518e58
shaunsephton <shaunsephton@gmail.com>
2010-07-22 17:02:27 +0200

    added rem and done, corrections

--- social/urls.py
+++ social/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns(
-    'social.views',
-    url(r'^add_friend/(?P<user_id>\d+)/$', 'add_friend', name='social_add_friend'),
+urlpatterns = patterns('',
+    url(r'^add-friend/(?P<user_id>\d+)/$', 'social.views.add_friend', name='social_add_friend'),
+    url(r'^remove-friend/(?P<user_id>\d+)/$', 'social.views.remove_friend', name='social_remove_friend'),
+    url(r'^invitation-confirm/done/$', 'social.views.invitation_confirm_done', name='social_invitation_confirm_done'),
+    url(r'^invitation-confirm/(?P<invitation_id>\d+)/$', 'friends.views.respond_to_friendship_invitation', {'redirect_to_view': 'social_invitation_confirm_done'}, name='social_invitation_confirm'),
 )

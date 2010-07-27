bc82953ac8bf36fbbf156fc7cfb4c6be308d2bd4
shaunsephton <shaunsephton@gmail.com>
2010-07-27 15:21:26 +0200

    beginings of wall comment post

--- social/utils.py
+++ social/utils.py
@@ -25,3 +25,26 @@ def filter_permitted_objects(object_list, owner, requesting_user, count=None):
                     break
 
     return filtered_list
+
+
+def put_wall_post_comment(sender, instance, created, **kwargs):
+    import pdb; pdb.set_trace()
+
+    posting_user = instance.user
+
+
+    
+    attch = { 
+        "name": "Link name", 
+        "link": "http://www.example.com/",
+        "caption": "{*actor*} posted a new review",
+        "description": "This is a longer description of the attachment",
+        "picture": "http://www.example.com/thumbnail.jpg"
+    }
+
+    context['request'].facebook.graph.put_wall_post('message', attachment=attch)
+
+from django.db.models.signals import post_save
+from django.contrib.comments.models import Comment
+
+post_save.connect(put_wall_post_comment, sender=Comment)

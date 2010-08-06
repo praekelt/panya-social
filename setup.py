aa6ef142f8e463580ed5555e1418703342f854b2
shaunsephton <shaunsephton@gmail.com>
2010-08-06 09:02:25 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='panya-social',
-    version='0.0.1',
+    version='0.0.2',
     description='Panya social app.',
     long_description = open('README.rst', 'r').read(),
     author='Praekelt Foundation',

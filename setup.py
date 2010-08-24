53ffeae848e7be7737d67f9bdf561446f676a2d7
shaunsephton <shaunsephton@gmail.com>
2010-08-24 10:01:20 +0200

    syntax error correction

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='panya-social',
     version='0.0.3',
     description='Panya social app.',
-    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read()
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
     author_email='dev@praekelt.com',
     license='BSD',

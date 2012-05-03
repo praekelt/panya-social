b2b346c5a9b2ebed7ebaebef7bba8641052653ed
Euan Jonker <euan@unomena.com>
2012-05-03 21:35:23 +0200

    Integrated the missing facebook api.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='panya-social',
-    version='0.0.5',
+    version='0.0.6',
     description='Panya social app.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
@@ -12,7 +12,6 @@ setup(
     packages = find_packages(),
     dependency_links = [
         'http://github.com/praekelt/django-socialregistration/tarball/0.4.1.tokenstore.1#egg=django-socialregistration-0.4.1.tokenstore.1',
-        'http://github.com/facebook/python-sdk/tarball/master#egg=facebook-python-sdk',
         'http://github.com/praekelt/django-activity-stream/tarball/master#egg=django-activity-stream-0.2.1.praekelt',
         'http://github.com/pinax/django-friends/tarball/master#egg=django-friends-0.2.dev1',
         'http://github.com/downloads/praekelt/public-eggs/django_notification-0.2.0.dev2-py2.6.egg=django-notification',
@@ -22,7 +21,6 @@ setup(
         'django-friends==0.2.dev1',
         'django-notification',
         'django-socialregistration==0.4.1.tokenstore.1',
-        'facebook-python-sdk',
         'panya',
     ],
     include_package_data=True,
